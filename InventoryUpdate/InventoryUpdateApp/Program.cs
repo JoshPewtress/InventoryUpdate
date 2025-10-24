a57cf772b409bd57b968499c03a917eb58396ca2
Josh Pewtress <61856857+JoshPewtress@users.noreply.github.com>
2025-10-24 14:42:32 -0600

    feat: add UI Search Methods

--- InventoryUpdate/InventoryUpdateApp/Program.cs
+++ InventoryUpdate/InventoryUpdateApp/Program.cs
@@ -1,4 +1,30 @@
-﻿
+﻿using InventoryUpdateApp;
 
+public class UIMethods
+{
+    public static void ListAllProducts()
+    {
+        Helper.GetAllItems().Print();
+    }
 
+    public static void ListProductById()
+    {
+        bool validId;
+        int id;
 
+        do
+        {
+            Console.Write("Enter the Product ID: ");
+            validId = int.TryParse(Console.ReadLine(), out id);
+
+        } while (validId == false);
+
+        Helper.GetItemById(id).Print();
+    }
+
+    public static void ListProductByLocation()
+    {
+        Console.Write("Enter the Location to search: ");
+        Helper.GetItemsByLocation(Console.ReadLine()).Print();
+    }
+}
