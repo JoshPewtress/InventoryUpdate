f527a0a21a08551fd8ca6325fa3deb12b8892a3b
Josh Pewtress <61856857+JoshPewtress@users.noreply.github.com>
2025-10-24 15:00:43 -0600

    feat: Add Greeting and Decision Tree

--- InventoryUpdate/InventoryUpdateApp/Program.cs
+++ InventoryUpdate/InventoryUpdateApp/Program.cs
@@ -2,6 +2,61 @@
 
 public class UIMethods
 {
+    public static void Greeting()
+    {
+        Console.WriteLine(
+            $"""
+            Welcome to the Inventory Update Application.
+
+            """);
+    }
+
+    public static void DecisionTree()
+    {
+        while (true)
+        {
+            Console.WriteLine(
+            $"""
+            Select an option...
+            1 - List all Products
+            2 - Search by ID
+            3 - Search by Location
+            4 - Clear Console
+            5 - Exit Application
+            """);
+
+            Console.Write("Selection: ");
+            int selection = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            switch (selection)
+            {
+                case 1:
+                    ListAllProducts();
+                    Console.WriteLine();
+                    break;
+                case 2:
+                    ListProductById();
+                    Console.WriteLine();
+                    break;
+                case 3:
+                    ListProductByLocation();
+                    Console.WriteLine();
+                    break;
+                case 4:
+                    Console.Clear();
+                    DecisionTree();
+                    break;
+                case 5:
+                    Environment.Exit(0);
+                    return;
+                default:
+                    Console.WriteLine("Invalid selection. Try again.");
+                    break;
+            } 
+        }
+    }
+
     public static void ListAllProducts()
     {
         Helper.GetAllItems().Print();
